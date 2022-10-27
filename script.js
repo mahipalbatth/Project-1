a3082ed89ac856158ae0fd932cc87ee9463e34b2
Mahipal Signh <msbatth14@gmail.com>
2022-10-27 16:42:20 -0500

    fetching beer object using beer API

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("testing beer");
+fetch("https://api.punkapi.com/v2/beers")
+.then(function (response) {
+    return response.json();
+})
+.then(function (bigBeerObject) {
+    console.log(bigBeerObject)
+});
