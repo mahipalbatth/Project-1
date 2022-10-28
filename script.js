405d23c1ac87766966a4ed8229d5ba41b4867f6b
Mahipal Signh <msbatth14@gmail.com>
2022-10-27 20:27:58 -0500

    random beer gereator with event listner

--- script.js
+++ script.js
@@ -24,3 +24,30 @@ fetch("https://api.punkapi.com/v2/beers")
      console.log("my custom arrys", names, food, images);
 
 });
+
+//wait for dom to be loaded and beer object fetched before button can work
+document.addEventListener("DOMContentLoaded", function(){
+    //click event listner
+    document.getElementById("randomButton").addEventListener("click", randomBeerGenerator);
+})
+
+
+//function that determines how many hours left in the day and suggests a random beer from big beer object
+function randomBeerGenerator() {
+    // using var instead of const because i want to reassign a value to hours left
+    var hoursLeft = new Date();
+    console.log(hoursLeft, hoursLeft.getHours());
+    hoursLeft = 24 - hoursLeft.getHours();
+
+    //get random beer 
+    const randomIndex = randomNumberGenerator(names.length);
+    const randomBeer = names[randomIndex];
+
+    //construct the message
+    document.getElementById("randomBeerSuggestion").innerHTML= "There's only " + hoursLeft + " hour(s) left for today! Mahipal's in the mood for some " +randomBeer +"!";
+}
+
+// function that gives a random array index not including the ending index
+function randomNumberGenerator(endingIndex) {
+    return Math.floor(Math.random() * (endingIndex));
+}
