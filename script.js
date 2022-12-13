5b6f573943d23425b18b475b91c44b521e6e3c45
Mahipal Signh <msbatth14@gmail.com>
2022-12-13 15:31:25 -0600

    Cleaing up

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 console.log("testing beer");
 
-//creating arrays i need from big beer object
+
 const names = [];
 const food = [];
 const images = [];
 
-//fetching beer object
+
 fetch("https://api.punkapi.com/v2/beers")
 .then(function (response) {
     return response.json();
@@ -15,7 +15,7 @@ fetch("https://api.punkapi.com/v2/beers")
 
     beers.forEach((beer, i) => {
 
-        //constructing my custom arrays
+        
         names[i] = beer.name;
         food[i] = beer.food_pairing;
         images[i] = beer.image_url;
@@ -28,67 +28,61 @@ fetch("https://api.punkapi.com/v2/beers")
 
 
 //adding EVENT LISTNERS
-//wait for dom to be loaded and beer object fetched before button can work
+
 document.addEventListener("DOMContentLoaded", callEventListeners);
 
 function callEventListeners() {
 
-    //click event listner
+    
     document.getElementById("randomButton").addEventListener("click", randomBeerGenerator);
 
-    //key press event listner
+    
     document.addEventListener("keypress", randomMenuGenerator); 
 
-    //touch event listner
-    document.getElementById("beerArt").addEventListener("touchstart", randomArtGenerator);
+    
+    document.getElementById("beerArt").addEventListener("touchstart", generateNewArtwork);
 
 
 }
 
-//Generate content for beer and food paragraphs 
+ 
 function randomMenuGenerator() {
 
-    //generate random index
+    
     const pickedObjectIndex = randomNumberGenerator(names.length);
 
-    //generate text for menu section
+   
     document.getElementById('beerName').innerHTML= 'Serving up some ' + names[pickedObjectIndex] + '?';
     document.getElementById('foodName').innerHTML= 'Well try these foods: ' + food[pickedObjectIndex];
 }
 
 
-//function be used with touch event listener and for clicking too
-function randomArtGenerator() {
-    generateNewArtwork();
 
-}
-
-
-
-//function that determines how many hours left in the day and suggests a random beer from big beer object
 function randomBeerGenerator() {
     
     let hoursLeft = new Date();
     console.log(hoursLeft, hoursLeft.getHours());
     hoursLeft = 24 - hoursLeft.getHours();
 
-    //get random beer 
+   
     const randomIndex = randomNumberGenerator(names.length);
     const randomBeer = names[randomIndex];
 
-    //construct the message
+    
     document.getElementById("randomBeerSuggestion").innerHTML= "There's only " + hoursLeft + " hour(s) left for today! Mahipal's in the mood for some " +randomBeer +"!";
 }
 
-// function that gives a random array index not including the ending index
+
 function randomNumberGenerator(endingIndex) {
     return Math.floor(Math.random() * (endingIndex));
 }
 
-//function that generates and updates random image art
+
 function generateNewArtwork() {
 
+    
     document.getElementById('beerArt').src=images[randomNumberGenerator(images.length)];
     console.log('wana show this', images[randomNumberGenerator(images.length)])
+    
 }
 
